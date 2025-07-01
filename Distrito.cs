6dacdbef2a5551d5de16b2a526ad77932aaa9f7a
Jesus Almirco <40370667+almircoo@users.noreply.github.com>
2025-06-30 19:59:35 -0700

    class distrito

---
+++ Distrito.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace POOI_web300625
+{
+    public class Distrito
+    {
+        public int code { get; set; }
+        public string nombre_distrito { get; set; }
+
+        public Distrito() { }
+    }
+}
