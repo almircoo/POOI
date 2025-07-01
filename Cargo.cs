32f543b2027c2cea7462e135152ed9f5d7f8471a
Jesus Almirco <40370667+almircoo@users.noreply.github.com>
2025-06-30 20:00:05 -0700

    clas cargo

---
+++ Cargo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace POOI_web300625
+{
+    public class Cargo
+    {
+        public int code {  get; set; }
+        public string descripcion { get; set; }
+        public int estado { get; set; }
+        public Cargo() { }
+    }
+    
+
+
+}
